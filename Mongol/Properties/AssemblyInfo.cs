972e0ac5e014065b4235eaadb645a4e2af20e432
Phillip Markert <phillip@ephisys.com>
2012-01-30 15:24:42 -0500

    Signed-off-by: Phillip Markert <phillip@ephisys.com>

--- Mongol/Properties/AssemblyInfo.cs
+++ Mongol/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.10.4.*")]
-[assembly: AssemblyInformationalVersion("0.11.1-alpha2")]
+[assembly: AssemblyVersion("0.2.0.*")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
